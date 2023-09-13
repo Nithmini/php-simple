de5182a4602cf030d25d7fb2d2c2248bce764399
Nithmini <nithminis@gmail.com>
2023-09-13 13:54:03 +0530

    Final

--- calculation.php
+++ calculation.php
@@ -26,6 +26,7 @@
 </form>
 
 <?php
+// ... (previous code) ...
 $hostname = "localhost"; // e.g., "localhost"
 $username = "root";
 $password = "mysql";
@@ -36,6 +37,24 @@ $conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+function calculateResult($number1, $number2, $operation) {
+    switch ($operation) {
+        case "Add":
+            return $number1 + $number2;
+        case "Subtract":
+            return $number1 - $number2;
+        case "Multiply":
+            return $number1 * $number2;
+        case "Divide":
+            if ($number2 != 0) {
+                return $number1 / $number2;
+            } else {
+                return "Division by zero is undefined.";
+            }
+        default:
+            return "Invalid operation!";
+    }
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["getHistory"])) {
@@ -45,9 +64,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if ($historyResult->num_rows > 0) {
             echo "<h2>Calculation History</h2>";
-            echo "<table border='1'><tr><th>Number 1</th><th>Number 2</th><th>Operation</th><th>Result</th></tr>";
+            echo "<table border='1'><tr><th>Number 1</th><th>Number 2</th><th>Operation</th><th>Result</th><th>Edit</th></tr>";
             while ($row = $historyResult->fetch_assoc()) {
-                echo "<tr><td>".$row["number1"]."</td><td>".$row["number2"]."</td><td>".$row["operation"]."</td><td>".$row["result"]."</td></tr>";
+                echo "<tr><td>".$row["number1"]."</td><td>".$row["number2"]."</td><td>".$row["operation"]."</td><td>".$row["result"]."</td>";
+                echo "<td><form method='post' action=''>
+                          <input type='hidden' name='editId' value='".$row["ID"]."'>
+                          <input type='submit' name='editRecord' value='Edit'>
+                      </form></td></tr>";
             }
             echo "</table>";
         } else {
@@ -55,12 +78,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     } elseif (isset($_POST["clearHistory"])) {
         // Code to clear calculation history
-        $clearHistoryQuery = "DELETE FROM details";
+         $clearHistoryQuery = "DELETE FROM details";
         if ($conn->query($clearHistoryQuery) === TRUE) {
             echo "Calculation history cleared.";
         } else {
             echo "Error clearing calculation history: " . $conn->error;
         }
+    } elseif (isset($_POST["editRecord"])) {
+        // Code to edit a specific record
+          $editId = $_POST["editId"];
+    
+    $editQuery = "SELECT * FROM details WHERE ID = $editId";
+    $editResult = $conn->query($editQuery);
+    if ($editResult->num_rows == 1) {
+        $row = $editResult->fetch_assoc();
+        // Display an edit form with the record's values
+        echo "<h2>Edit Calculation Record</h2>";
+        echo "<form method='post' action=''>
+                  Number 1: <input type='text' name='editedNumber1' value='".$row["number1"]."'><br><br>
+                  Number 2: <input type='text' name='editedNumber2' value='".$row["number2"]."'><br><br>
+                  Operation: <input type='text' name='editedOperation' value='".$row["operation"]."'><br><br>
+                  Result: <input type='text' name='editedResult' value='".$row["result"]."' readonly><br><br>
+                  <input type='hidden' name='editId' value='$editId'>
+                  <input type='submit' name='saveEdit' value='Save Edit'>
+              </form>";
+    } else {
+        echo "Record not found for editing.";
+    }
+    } elseif (isset($_POST["saveEdit"])) {
+        // Code to save the edited record
+        $editId = $_POST["editId"];
+    $editedNumber1 = $_POST["editedNumber1"];
+    $editedNumber2 = $_POST["editedNumber2"];
+    $editedOperation = $_POST["editedOperation"];
+    // Calculate the result based on the edited numbers and operation
+    $editedResult = calculateResult($editedNumber1, $editedNumber2, $editedOperation);
+
+    // Perform the update query to save the edited values and calculated result
+    $updateQuery = "UPDATE details SET number1 = '$editedNumber1', number2 = '$editedNumber2', operation = '$editedOperation', result = '$editedResult' WHERE id = $editId";
+    if ($conn->query($updateQuery) === TRUE) {
+        echo "Record updated successfully.";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
     } else {
         $number1 = $_POST["number1"];
         $number2 = $_POST["number2"];
@@ -70,26 +130,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (!is_numeric($number1) || !is_numeric($number2)) {
             echo "Invalid input. Please enter valid numbers.";
         } else {
-            if ($operation == "Add") {
-                $result = $number1 + $number2;
-            } elseif ($operation == "Subtract") {
-                $result = $number1 - $number2;
-            } elseif ($operation == "Multiply") {
-                $result = $number1 * $number2;
-            } elseif ($operation == "Divide") {
-                if ($number2 != 0) {
-                    $result = $number1 / $number2;
-                } else {
-                    echo "Division by zero is undefined.";
-                    $result = null; // Set result to null to avoid inserting an invalid result
-                }
-            } else {
-                echo "Invalid operation!";
-                $result = null; // Set result to null to avoid inserting an invalid result
-            }
+            $result = calculateResult($number1, $number2, $operation);
 
             // Insert the calculation result into the database
-            if ($result !== null) { // Only insert if the result is valid
+            if (is_numeric($result)) { // Only insert if the result is numeric (not an error message)
                 $sql = "INSERT INTO details (number1, number2, operation, result) VALUES ('$number1','$number2','$operation','$result')";
 
                 if ($conn->query($sql) === TRUE) {
@@ -97,11 +141,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 } else {
                     echo "Error inserting data into the database: " . $conn->error;
                 }
+            } else {
+                echo $result; // Display error message
             }
         }
     }
 }
 ?>
-
 </body>
 </html>
